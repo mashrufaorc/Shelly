47ab8819c77c5507d9b8801ad1274b2f695a15cf
tckillam <tckillam@gmail.com>
2026-02-18 13:53:12 -0500

    Update a2.ino
    attempting to just make the robot stay in bounds

--- a2/a2.ino
+++ a2/a2.ino
@@ -22,14 +22,26 @@ void setup() {
 void loop() {
 
   int speed = 150;
-
+  
   int lineColor = digitalRead(lineTrack); // 0:white  1:black
   Serial.println(lineColor); //print on the serial monitor
+
+  // this is the original code - telling the robot to micro turn and continue forward everytime it hits the tape
   if (lineColor) {
-    moveLeft(speed);
+  moveLeft(speed);
   } else {
     moveRight(speed);
   }
+
+  // so i am thinking, if it does not detect tape, go forward
+  // and if it does detect tape, maybe we can place one sensor on the right so it then turns left?
+  //if (lineColor) {
+  //  moveLeft(speed);
+  //} else {
+  //  moveForward(speed);
+  //}
+}
+
 }
 void moveLeft(int speed) {
   analogWrite(A_1B, 0);
@@ -43,4 +55,11 @@ void moveRight(int speed) {
   analogWrite(A_1A, 0);
   analogWrite(B_1B, speed);
   analogWrite(B_1A, 0);
+}
+
+void moveForward(int speed) {
+  analogWrite(A_pin1, speed);
+  digitalWrite(A_pin2, LOW);
+  analogWrite(B_pin1, speed);
+  digitalWrite(B_pin2, LOW);
 }
